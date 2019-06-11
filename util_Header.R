9cff0b7df3c262719a3ea553d060f765fa0d7b7b
shringi <ankurshringi@iisc.ac.in>
2019-06-11 17:35:00 +0530

    Created a new header style which usage the render() function to give output as html notebook. Reduces a clutter of code compare to previous header snippet (see file R-Header-yy.txt).

--- util_Header.R
+++ util_Header.R
@@ -1,60 +1,16 @@
-#' ---
-#' title: util_Header.R
-#' subtitle: |
-#'
-#'author:
-#'  - name: Ankur Shringi
-#'    email: ankurshringi@iisc.ac.in
-#'    affiliation: Centre for Ecological Sciences, Indian Institute of Science, Bangalore, India
-#'abstract: |
-#'
-#'output:
-#'  html_document:
-#'    self_contained: true
-#'    toc: true
-#'    toc_depth: 2
-#'    number_sections: true
-#'    toc_type: float
-#'    toc_float: true
-#'    highlight: kate
-#'    toc_collapsed: false
-#'---
-#'*****
-#' Project: util_Global <br><br> Enter Following command to execute it. <br>
-#' `export2html('util_Header.R', folder = 'Output-R-Html', suppress_warnings = TRUE, browse = TRUE)` <br><br>
-#' Date created: <br> 2019-Jan-09 20:49:01 Wednesday <br><br>
-#+ echo = FALSE
-# ================================================================ --------
-#'*****
-#' # Initialization
-#+ echo = FALSE
-# Loading utility function used by Ankur ----------------------------------
-#' ## Loading utility function used by Ankur
+# Author: Ankur Shringi (ankurshringi@iisc.ac.in)
+# title: util_Header.R
+# subtitle: ...
+# abstract: ...
+# Project: util_Global
+# Date created: 2019-Jun-11 17:04:06 Tuesday
+# Enter following command to render the code as html
+# `render()`
+# Initialization ----------------------------------------------------------
+# Loading custom made utility functions
 source("C:/Users/Ankur/hubiC/Work/Projects/Utilities/util_Global/util_Global.R")
-#+ echo = FALSE
-# Deleting R-Environment Variables (Except utility functions) -------------
-#' ## Deleting R-Environment Variables (Except utility functions)
+# Deleting R-Environment Variables (Except utility functions)
 clr()
-#+ echo = FALSE
-# Loading required packages -----------------------------------------------
-#'## Loading required packages
-Packages <-     c("tidyverse","roxygen2Comment")
+# Loading required packages
+Packages <-     c("tidyverse", "roxygen2Comment")
 install(Packages); rm(Packages)
-#+ echo = FALSE
-# Start of the real coding ------------------------------------------------
-#'*****
-#' # Start of the real coding
-#+ echo = FALSE
-# ---------------------------------------------------------------- --------
-
-#+ echo = FALSE
-# ---------------------------------------------------------------- --------
-#+ echo = FALSE
-# End of the coding -------------------------------------------------------
-#'*****
-#' # Termination
-#+ echo = FALSE
-# ================================================================ --------
-# Session Info ------------------------------------------------------------
-#' ## Session Info
-# runInfo()
