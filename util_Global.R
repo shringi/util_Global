055be3a9b53fbaaca16a6d931b444f287a69b3b1
shringi <ankurshringi@iisc.ac.in>
2020-06-01 21:01:31 +0530

    Added paste.adv() function. This can ignore NA values while collapsing the strings.

--- util_Global.R
+++ util_Global.R
@@ -1218,3 +1218,30 @@ files.status <- function(folder_check = "01-Data", folder_out = "03-Tables", wri
   }
 }
 
+
+# paste.adv() -------------------------------------------------------------------------------------------
+# paste which can ignore NA values
+# taken from https://stackoverflow.com/questions/13673894/suppress-nas-in-paste
+paste.adv <- function(..., sep = " ", collapse = NULL, na.rm = F) {
+if (na.rm == F)
+  paste(..., sep = sep, collapse = collapse)
+else
+  if (na.rm == T) {
+    paste.na <- function(x, sep) {
+      x <- gsub("^\\s+|\\s+$", "", x)
+      ret <- paste(na.omit(x), collapse = sep)
+      is.na(ret) <- ret == ""
+      return(ret)
+    }
+    df <- data.frame(..., stringsAsFactors = F)
+    ret <- apply(df, 1, FUN = function(x) paste.na(x, sep))
+
+    if (is.null(collapse))
+      ret
+    else {
+      paste.na(ret, sep = collapse)
+    }
+  }
+}
+
+
